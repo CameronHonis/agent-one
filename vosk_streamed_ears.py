2b7b941788b8c32005eeca0c387692f4a6de58be
cameron <cameron-04@hotmail.com>
2025-03-14 15:49:37 -0500

    vosk ears w/ active|passive modes

--- vosk_streamed_ears.py
+++ vosk_streamed_ears.py
@@ -1,5 +1,7 @@
+from enum import Enum
 import json
 import logging
+import math
 import multiprocessing as mp
 import time
 from typing import override
@@ -10,6 +12,7 @@ from vosk import Model, KaldiRecognizer
 
 PAUSE_THRESHOLD_SECS = 2  # amount of time to allow pass with no additional incoming words before sending prompt
 AUDIO_SAMPLE_RATE = 16_000  # 16kHz works best with vosk
+TRIGGER = "hey agent"
 
 logger = logging.getLogger(__name__)
 
@@ -39,6 +42,11 @@ class Recognizer(KaldiRecognizer):
         return result
 
 
+class ListeningMode(Enum):
+    PASSIVE = "passive"
+    ACTIVE = "active"
+
+
 class VoskStreamedEars(Ears):
     def __init__(self, model_path: str):
         super().__init__()
@@ -47,8 +55,9 @@ class VoskStreamedEars(Ears):
         self._manager = mp.Manager()
         self._shared_audio_q = self._manager.Queue()
         self._shared_last_audio_time = self._manager.Value("d", None)
-        self._shared_prompts = self._manager.list()
+        self._shared_words = self._manager.list()
         self._shared_timer_active = self._manager.Value("b", False)
+        self._shared_listening_mode = self._manager.Value("s", ListeningMode.PASSIVE)
         self._lock = self._manager.Lock()
 
     def listen(self) -> mp.Process:
@@ -89,57 +98,120 @@ class VoskStreamedEars(Ears):
         self._shared_audio_q.put(bytes(audio_data))
 
     def _process_audio(self, audio_bytes: bytes, recognizer: Recognizer):
-        with self._lock:
-            if recognizer.AcceptWaveform(audio_bytes):
-                full = recognizer.get_full()
-                logger.debug("heard: %s", full)
+        is_final = recognizer.AcceptWaveform(audio_bytes)
+        if is_final:
+            full = recognizer.get_full()
+            self._handle_words(full)
+        else:
+            partial = recognizer.get_partial()
+            if partial:
+                self._handle_partial(partial)
 
-                if not self._shared_timer_active.value:
-                    self._shared_timer_active.value = True
-                    proc = mp.Process(target=self._prompter_clock)
-                    proc.start()
-                self._shared_last_audio_time.value = time.time()
-            else:
-                partial = recognizer.get_partial()
-                if partial:
-                    logger.debug("heard partial: %s", partial)
-                    self._shared_last_audio_time.value = time.time()
+    def _try_start_prompter_clock(self):
+        with self._lock:
+            if self._shared_timer_active.value:
+                return
+            self._shared_timer_active.value = True
+        logger.debug("starting clock")
+        mp.Process(target=self._prompter_clock).start()
 
     def _prompter_clock(self):
         while True:
             logger.debug("running prompt clock")
+            to_send_prompt = False
             with self._lock:
                 now = time.time()
                 dt_secs = now - self._shared_last_audio_time.value
                 logger.debug("seconds since last word spoken: %s", dt_secs)
-                if dt_secs >= PAUSE_THRESHOLD_SECS and len(self._shared_prompts):
-                    self._send_prompt()
-                    self._shared_timer_active.value = False
-                    return
-            wait_secs = PAUSE_THRESHOLD_SECS - dt_secs
+                if dt_secs >= PAUSE_THRESHOLD_SECS and len(self._shared_words):
+                    to_send_prompt = True
+
+            if to_send_prompt:
+                prompt_sent = self._init_prompt()
+
+                if prompt_sent:
+                    logger.debug("prompt sent, killing clock")
+                    with self._lock:
+                        self._shared_timer_active.value = False
+                        return
+                else:
+                    logger.debug("prompt couldn't be sent, keeping clock alive")
+
+            wait_secs = max(PAUSE_THRESHOLD_SECS - dt_secs, 1)
             time.sleep(wait_secs)
 
-    def _send_prompt(self):
+    def _handle_partial(self, words: str):
+        """
+        handles words that have not yet been finalized by the transcriber
+        """
+        logger.debug("partial: %s", words)
+        with self._lock:
+            self._shared_last_audio_time.value = time.time()
+            is_passive = self._shared_listening_mode.value == ListeningMode.PASSIVE
+            if is_passive and TRIGGER in words:
+                self._shared_listening_mode.value = ListeningMode.ACTIVE
+                logger.info("actively listening...")
+
+    def _handle_words(self, words: str):
+        """
+        handles words that have been finalized by the transcriber
+        """
+        logger.debug("heard: %s", words)
+
+        with self._lock:
+            self._shared_words.append(words)
+            self._shared_last_audio_time.value = time.time()
+            is_active = self._shared_listening_mode.value == ListeningMode.ACTIVE
+            if (not is_active) and TRIGGER in words:
+                self._shared_listening_mode.value = ListeningMode.ACTIVE
+                logger.info("actively listening...")
+                is_active = True
+        if is_active:
+            self._try_start_prompter_clock()
+
+    def _build_prompt(self) -> str:
+        """
+        iterates through recorded words in reverse order until full prompt is built, excludes trigger words
+        """
+        with self._lock:
+            words = " ".join(self._shared_words)
+        trigger_match_idx = words.rfind(TRIGGER)
+        if trigger_match_idx == -1:
+            raise ValueError("Trigger was never spoken, cannot built prompt")
+        return words[trigger_match_idx + len(TRIGGER) :]
+
+    def _init_prompt(self) -> bool:
         """
         assumes that audio_q has been fully processed.
         it should, unless transcription of a single block took longer than the required
         pause before sending prompt.
         if not, though, this function will raise a ValueError.
+
+        returns True if prompt could be built, otherwise returns False
         """
+        try:
+            prompt = self._build_prompt()
+        except Exception:
+            pass
+        if not prompt:
+            return False
+
+        logger.debug("built prompt: %s", prompt)
         with self._lock:
-            if len(self._shared_audio_q):
+            if not self._shared_audio_q.empty():
                 raise ValueError(
                     "tried sending prompt when audio queue is still populated"
                 )
-            prompt = " ".join(self._shared_prompts)
-            logger.debug("built prompt: %s", prompt)
+
             if self.agent:
                 self.agent.prompt(prompt)
             else:
                 logger.warning("no agent connected, failed to send prompt")
 
-            self._shared_prompts.clear()
-            self._shared_audio_q.empty()
+            self._shared_words[:] = []
+            self._shared_listening_mode.value = ListeningMode.PASSIVE
+            logger.debug("passively listening...")
+            return True
 
 
 if __name__ == "__main__":
